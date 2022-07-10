883eb8ca5b53ede5265fef22a0c5536df8cd4676
Qinghuan <1041220793@qq.com>
2022-07-10 21:40:49 +0800

    feat: init plugin options

--- examples/vite-vue3/vite.config.ts
+++ examples/vite-vue3/vite.config.ts
@@ -3,6 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import UnpluginAutoMeta from 'unplugin-auto-meta'
 
 // https://vitejs.dev/config/
+
+const options = {
+  names: [
+    { key: 'xx', content: 'xxx' }
+  ],
+  httpEquivs: [
+    { key: 'xx', content: 'xxx' },
+    { key: 'xx', content: 'xxx' }
+  ]
+}
+
 export default defineConfig({
-  plugins: [vue(), UnpluginAutoMeta()]
+  plugins: [vue(), UnpluginAutoMeta(options)]
 })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import type { PluginOption } from 'vite';
+import type { PluginOption, HtmlTagDescriptor } from 'vite';
 
-export default function createUnplugin(): PluginOption {
+export interface MetaOption {
+  key: string
+  content: string
+}
+
+export interface MetaPluginOptions {
+  names?: MetaOption[]
+  httpEquivs?: MetaOption[]
+  charset?: string
+}
+
+export default function UnpluginAutoMeta(options?: MetaPluginOptions): PluginOption {
   return {
     name: 'unplugin-auto-meta',
     enforce: 'pre',
     apply: 'build',
 
-    transformIndexHtml(html) {
-      console.log(html);
+    transformIndexHtml(html: string) {
+      const tags: HtmlTagDescriptor[] = []
+
+      const tag: HtmlTagDescriptor = {
+        tag: 'meta',
+        attrs: {
+          name: 'author',
+          content: 'qinghuan'
+        },
+        injectTo: 'head'
+      }
+      tags.push(tag)
+      return tags
     },
   }
 }
